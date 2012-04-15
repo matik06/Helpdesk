44dc11a39d29cf0c5a96d3313a0db1bcc0e40c94
Mateusz Lubańśki <mlubanskii@gmail.com>
2012-04-15 16:30:32 +0200

    basic configuration

---
+++ ssh-util/src/main/java/pl/helpdesk/sshutil/App.java
@@ -0,0 +1,13 @@
+package pl.helpdesk.sshutil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
