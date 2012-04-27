84484adc43e9506801294993af20d5b48042c377
Mateusz Lubański <mlubanskii@gmail.com>
2012-04-27 21:58:49 +0200

    test

--- ssh-util/src/main/java/pl/helpdesk/sshutil/App.java
+++ ssh-util/src/main/java/pl/helpdesk/sshutil/App.java
@@ -1,13 +1,51 @@
 package pl.helpdesk.sshutil;
 
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 /**
- * Hello world!
+ * http://www.time4tea.net/wiki/display/MAIN/SCP+a+file+in+Java+using+JSch
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    private static final String USER_NAME = "matik06";
+    private static final String HOST = "d3vil0.no-ip.org";
+    private static final int PORT = 22;
+    private static final String PASSWORD = "zzxxcc";
+    
+    public static void main( String[] args ) throws JSchException, IOException, SftpException
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Test połączenia ssh: ");
+        
+        JSch jsch = new JSch();
+        Session session = jsch.getSession(USER_NAME, HOST, PORT);
+        session.setPassword(PASSWORD);        
+        session.setConfig("StrictHostKeyChecking", "no");
+        
+        session.connect();
+                
+        boolean isConnected = session.isConnected();
+        System.out.println(isConnected);
+        
+        
+        
+        //**********************************************************************/
+        File localFile = new File("/home/matik/test_result.txt");
+        ChannelSftp channel = (ChannelSftp) session.openChannel("sftp");
+        channel.connect();
+        channel.cd("./");
+        channel.put(new FileInputStream(localFile), "test2.txt");
+        channel.disconnect();
+        /**********************************************************************/
+        
+        session.disconnect();        
+        System.out.println("Koniec testów :)");
     }
 }
--- web-app/src/main/java/pl/helpdesk/service/impl/GenericServiceImpl.java
+++ web-app/src/main/java/pl/helpdesk/service/impl/GenericServiceImpl.java
@@ -7,6 +7,7 @@ package pl.helpdesk.service.impl;
 import java.io.Serializable;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import pl.helpdesk.dao.GenericDao;
 import pl.helpdesk.service.GenericService;
 
@@ -24,31 +25,37 @@ public abstract class GenericServiceImpl<T, ID extends Serializable, DAO extends
     
 
     @Override
+    @Transactional(readOnly=true)
     public T findById(ID id) {
         return (T)dao.findById(id);
     }
 
     @Override
+    @Transactional(readOnly=true)
     public List<T> findAll(int stardIndex, int fetchSize) {
         return dao.findAll(stardIndex, fetchSize);
     }
 
     @Override
+    @Transactional(readOnly=true)
     public List<T> findByExample(T exampleInstance, String[] excludeProperty) {
         return dao.findByExample(exampleInstance, excludeProperty);
     }
 
     @Override
+    @Transactional()
     public T save(T entity) {
         return (T)dao.save(entity);
     }
 
     @Override
+    @Transactional()
     public T update(T entity) {
         return (T)dao.update(entity);
     }
 
     @Override
+    @Transactional()
     public void delete(T entity) {
         dao.delete(entity);
     }
