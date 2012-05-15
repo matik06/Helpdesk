27d2e31b61cfb4582ea4de0ebc1e2bede652fc98
Mateusz Lubański <mlubanskii@gmail.com>
2012-05-15 20:13:50 +0200

    konfiguracja log4j oraz opis uruchomienia tomcata po ssl'u

---
+++ web-app/src/main/java/Test.java
@@ -0,0 +1,21 @@
+
+import org.apache.log4j.Logger;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mateusz Lubański <mlubanskii@gmail.com>
+ */
+public class Test {
+    
+    private static final Logger logger = Logger.getLogger(Test.class);
+    
+    public static void main(String [] args) {
+        logger.info("test logów");
+    }
+    
+}
